615e8397fa4074692f4c201931c1adb8188f53dc
Corentin <corentin.lizy@ynov.com>
2022-04-14 16:29:17 +0200

    removing useless folders

---
+++ Ratz_API/Ratz_API/Controllers/UserController.cs
@@ -0,0 +1,16 @@
+﻿using DatabaseConnection;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Ratz_API.Controllers
+{
+    public class UserController : ControllerBase
+    {
+        private readonly IUserRepository _userRepository;
+
+        public UserController(IUserRepository iUserRepository)
+        {
+            _userRepository = iUserRepository;
+        }
+    }
+}
